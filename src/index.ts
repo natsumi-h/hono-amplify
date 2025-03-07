4f68fa2e406481b483a828c11ed06dd0120c2645
Natsumi.H <natsmy.1211@gmail.com>
2025-03-07 23:48:27 +0900

    c

--- src/index.ts
+++ src/index.ts
@@ -16,12 +16,14 @@ app.post("/", (c) => {
   return c.json(body);
 });
 
-serve(
-  {
-    fetch: app.fetch,
-    port: 3000,
-  },
-  (info) => {
-    console.log(`Server is running on http://localhost:${info.port}`);
-  }
-);
+// serve(
+//   {
+//     fetch: app.fetch,
+//     port: 3000,
+//   },
+//   (info) => {
+//     console.log(`Server is running on http://localhost:${info.port}`);
+//   }
+// );
+
+serve(app);
